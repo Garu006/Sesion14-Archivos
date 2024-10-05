f02047ab347e62a0afa1eff827abeed5ad05f9b8
Garu006 <garojas@uamv.edu.ni>
2024-10-04 18:20:36 -0600

    Archivos Visuales

---
+++ ArchivosVisuales/ArchivosVisuales/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace ArchivosVisuales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbEdad = new System.Windows.Forms.TextBox();
+            this.tbNota = new System.Windows.Forms.TextBox();
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.lbEdad = new System.Windows.Forms.Label();
+            this.lbGenero = new System.Windows.Forms.Label();
+            this.lbNota = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.dgvRegistros = new System.Windows.Forms.DataGridView();
+            this.cmbGender = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(135, 48);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(100, 20);
+            this.tbNombre.TabIndex = 0;
+            // 
+            // tbEdad
+            // 
+            this.tbEdad.Location = new System.Drawing.Point(135, 92);
+            this.tbEdad.Name = "tbEdad";
+            this.tbEdad.Size = new System.Drawing.Size(100, 20);
+            this.tbEdad.TabIndex = 1;
+            // 
+            // tbNota
+            // 
+            this.tbNota.Location = new System.Drawing.Point(135, 190);
+            this.tbNota.Name = "tbNota";
+            this.tbNota.Size = new System.Drawing.Size(100, 20);
+            this.tbNota.TabIndex = 3;
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Location = new System.Drawing.Point(13, 48);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(116, 13);
+            this.lbNombre.TabIndex = 4;
+            this.lbNombre.Text = "Nombre del estudiante:";
+            // 
+            // lbEdad
+            // 
+            this.lbEdad.AutoSize = true;
+            this.lbEdad.Location = new System.Drawing.Point(16, 95);
+            this.lbEdad.Name = "lbEdad";
+            this.lbEdad.Size = new System.Drawing.Size(104, 13);
+            this.lbEdad.TabIndex = 5;
+            this.lbEdad.Text = "Edad del estudiante:";
+            // 
+            // lbGenero
+            // 
+            this.lbGenero.AutoSize = true;
+            this.lbGenero.Location = new System.Drawing.Point(16, 143);
+            this.lbGenero.Name = "lbGenero";
+            this.lbGenero.Size = new System.Drawing.Size(114, 13);
+            this.lbGenero.TabIndex = 6;
+            this.lbGenero.Text = "Genero del estudiante:";
+            // 
+            // lbNota
+            // 
+            this.lbNota.AutoSize = true;
+            this.lbNota.Location = new System.Drawing.Point(19, 197);
+            this.lbNota.Name = "lbNota";
+            this.lbNota.Size = new System.Drawing.Size(102, 13);
+            this.lbNota.TabIndex = 7;
+            this.lbNota.Text = "Nota del estudiante:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(19, 241);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(79, 40);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(117, 241);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(87, 40);
+            this.btnMostrar.TabIndex = 9;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // dgvRegistros
+            // 
+            this.dgvRegistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRegistros.Location = new System.Drawing.Point(298, 53);
+            this.dgvRegistros.Name = "dgvRegistros";
+            this.dgvRegistros.Size = new System.Drawing.Size(240, 150);
+            this.dgvRegistros.TabIndex = 10;
+            // 
+            // cmbGender
+            // 
+            this.cmbGender.FormattingEnabled = true;
+            this.cmbGender.Items.AddRange(new object[] {
+            "M",
+            "F"});
+            this.cmbGender.Location = new System.Drawing.Point(136, 135);
+            this.cmbGender.Name = "cmbGender";
+            this.cmbGender.Size = new System.Drawing.Size(121, 21);
+            this.cmbGender.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbGender);
+            this.Controls.Add(this.dgvRegistros);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lbNota);
+            this.Controls.Add(this.lbGenero);
+            this.Controls.Add(this.lbEdad);
+            this.Controls.Add(this.lbNombre);
+            this.Controls.Add(this.tbNota);
+            this.Controls.Add(this.tbEdad);
+            this.Controls.Add(this.tbNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistros)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbEdad;
+        private System.Windows.Forms.TextBox tbNota;
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.Label lbEdad;
+        private System.Windows.Forms.Label lbGenero;
+        private System.Windows.Forms.Label lbNota;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.DataGridView dgvRegistros;
+        private System.Windows.Forms.ComboBox cmbGender;
+    }
+}
+
---
+++ ArchivosVisuales/ArchivosVisuales/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArchivosVisuales
+{
+
+    struct Persona
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public int grade { get; set; }
+        public char gender { get; set; }
+    }
+
+    public partial class Form1 : Form
+    {
+        List<Persona> lista = new List<Persona>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+
+
+            Persona persona = new Persona();
+            persona.name = tbNombre.Text;
+            persona.age = int.Parse(tbEdad.Text);
+            persona.grade = int.Parse(tbNota.Text);
+            if (cmbGender.SelectedIndex == 0)
+                persona.gender = 'M';
+            else
+            {
+                persona.gender = 'F';
+            }
+            //persona.gender = char.Parse(cmbGender.SelectedText.ToString());
+            lista.Add(persona);
+            mostrar();
+
+        }
+
+        public void mostrar()
+        {
+            dgvRegistros.DataSource = null;
+            dgvRegistros.DataSource = lista;
+        }
+
+        public void guardar()
+        {
+            FileStream mArchivoEscritor = new FileStream("datos.dat", FileMode.OpenOrCreate, FileAccess.Write);
+            using (BinaryWriter Escritor = new BinaryWriter(mArchivoEscritor))
+            {
+
+                foreach (Persona persona in lista)
+                {
+                    {
+                        Escritor.Write(persona.name.Length);
+                        Escritor.Write(persona.name.ToCharArray());
+                        Escritor.Write(persona.age);
+                        Escritor.Write(persona.grade);
+                        Escritor.Write(persona.gender);
+                    }
+
+                }
+            }
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            /*//listBox1.Items.Clear();
+
+            FileStream mArchivoLector = new FileStream("datos.dat", FileMode.Open, FileAccess.Read);
+            using (BinaryReader Lector = new BinaryReader(mArchivoLector))
+            {
+                while (mArchivoLector.Position != mArchivoLector.Length)
+                {
+                    int length = Lector.ReadInt32();
+                    char[] nombreArray = Lector.ReadChars(length);
+                    string nombre = new string(nombreArray);
+                    int edad = Lector.ReadInt32();
+                    int nota = Lector.ReadInt32();
+                    char genero = Lector.ReadChar();
+
+
+                    //listBox1.Items.Add($"Nombre: {nombre}, Edad: {edad}, Nota: {nota}, Género: {genero}");
+                }
+            }*/
+
+            guardar();
+        }
+        private void Mostrar()
+        {
+            tbNombre.Clear();
+            tbEdad.Clear();
+            tbNota.Clear();
+            //tbGenero.Clear();
+        }
+    }
+}
